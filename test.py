833dab53bbd1ed27c52389b679a2ab0f80a22a56
kuroda0722 <57052060+kuroda0722@users.noreply.github.com>
2019-10-27 11:48:27 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print(1+1)
+# coding:utf-8
+import cgitb
+import sys
+import io
+sys.stdin = open(sys.stdin.fileno(),  'r', encoding='UTF-8')
+sys.stdout = open(sys.stdout.fileno(), 'w', encoding='UTF-8')
+sys.stderr = open(sys.stderr.fileno(), 'w', encoding='UTF-8')
+cgitb.enable()
+ 
+print("Content-type: text/html; charset=utf-8")
+print("")
+bb = ccc
+aa = "aaa" + "bbbb"
+HtmlData = """
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>Hello World | python</title>
+</head>
+<body>
+<h1>Hello world for Python</h1>
+"""
+HtmlData+=aa
+HtmlData += """
+<h2>‚ ‚ ‚ ‚ </h2>
+</body>
+"""
+ 
+print(HtmlData)
