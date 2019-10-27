346dd2820f0560d9344fcfabee60ffb37cea2d84
kuroda0722 <57052060+kuroda0722@users.noreply.github.com>
2019-10-27 20:21:52 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -3,10 +3,37 @@
 
 from datetime import datetime
 from flask import Flask, request, Response, abort, render_template
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from collections import defaultdict
 import csv
 import os
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+
+# ログイン用ユーザー作成
+users = {
+    1: User(1, "user01", "password"),
+    2: User(2, "user02", "password"),
+}
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
+
+# ユーザーチェックに使用する辞書作成
+nested_dict = lambda: defaultdict(nested_dict)
+user_check = nested_dict()
+for i in users.values():
+    user_check[i.name]["password"] = i.password
+    user_check[i.name]["id"] = i.id
+
 
 class review_dt:
     def __init__(self):
@@ -147,6 +174,47 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+# ログインしないと表示されないパス
+@app.route('/protected/')
+@login_required
+def protected():
+    return Response('''
+    protected<br />
+    <a href="/logout/">logout</a>
+    ''')
+
+# ログインパス
+@app.route('/login/', methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        # ユーザーチェック
+        if(request.form["username"] in user_check and request.form["password"] == user_check[request.form["username"]]["password"]):
+            # ユーザーが存在した場合はログイン
+            login_user(users.get(user_check[request.form["username"]]["id"]))
+            return Response('''
+            ogin success!<br />
+            #<a href="/protected/">protected</a><br />
+            <a href="/logout/">logout</a>
+            ''')
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+
+# ログアウトパス
+@app.route('/logout/')
+#@login_required
+def logout():
+    logout_user()
+    
+    return Response('''
+    logout success!<br />
+    <a href="/login/">login</a>
+    ''')
+
+
 #@app.route('/')
 #def index():
 #    return "Hello World !"  
