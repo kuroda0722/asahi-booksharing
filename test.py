949a7a6868c3006da0841f55da813c64f3b99810
kuroda0722 <57052060+kuroda0722@users.noreply.github.com>
2019-10-27 17:46:24 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -32,36 +32,7 @@ class book_dt:
         self.reserver = ''
         self.date = ''
 
-file_list = os.listdir("./data")
-
-count = 0  
-book = []
-for file in file_list:
-    book_temp = book_dt()
-    with open('./data/'+file) as f:
-        reader = csv.reader(f)
-        print("aaaa")
-        print (reader)
-        for row in reader:
-            if(row[0] == 'title'):
-                print(row[1])
-                book_temp.title = row[1]
-            if(row[0] == 'img'):
-                book_temp.img = row[1]
-            if(row[0] == 'review'):
-                review_temp = review_dt()
-                review_temp.rank = row[1]
-                review_temp.name = row[2]
-                review_temp.text = row[3]
-                book_temp.review.append(review_temp)
-                print("☆" + review_temp.rank + review_temp.name +"さん曰く、"+ review_temp.text)
-            if(row[0] == 'lend'):
-                if (row[1] == '1'):
-                    print("貸し出し中")
-                else:
-                    print("いける")
-    book.append( book_temp)
-print(book)       
+     
 
 @app.route('/')
 def index():
