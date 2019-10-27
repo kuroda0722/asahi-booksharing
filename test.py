49a878438fb18a956c6beb24a974cc004027d4fa
kuroda0722 <57052060+kuroda0722@users.noreply.github.com>
2019-10-27 14:43:41 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print(1+1)
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    print(1+1)
+    return "Hello World !"
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
