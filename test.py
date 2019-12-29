6c1e492c4b89db998f4af3c603efa48d6e74aed0
kuroda0722 <57052060+kuroda0722@users.noreply.github.com>
2019-12-29 19:16:09 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -170,7 +170,7 @@ def bookpage(title = '',command = '',val=''):
 
                 line_notify_token = os.environ["LINE_TOKEN"]
                 line_notify_api = 'https://notify-api.line.me/api/notify'
-                line_message = '{0} {1} 予約キャンセルされました'.format(b.title,num,b.reserver)
+                line_message = '{0} {1} 予約キャンセルされました'.format(b.title,b.reserver)
 
 
                 payload = {'message': line_message}
